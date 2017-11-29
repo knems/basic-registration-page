01cb50ab56e8e2bac993ab57ad65e213a8c0ece9
Daniel Boisselle <dboisselle@hotmail.com>
2017-11-29 16:18:30 -0500

    finished styling and responiveness

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function(){
+    let stateSelect = document.getElementById('state');
+
+    stateSelect.style.color = '#97979e';
+}, false);
