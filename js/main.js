ca6ae3c73ac981cb1f4a6391c7f4ad378dfbc585
Daniel Boisselle <dboisselle@hotmail.com>
2017-12-01 10:06:08 -0500

    added focusing states to the label element when inputs are focused / other subtle styling effects

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@
 
 document.addEventListener('DOMContentLoaded', function(){
     let stateSelect = document.getElementById('state');
-
     stateSelect.style.color = '#97979e';
+
+		let inputs = document.getElementsByTagName('input');
+
+		for(let i=0; i < inputs.length; i++){
+			inputs[i].addEventListener('focusin', function(){
+				this.previousElementSibling.style.color = "unset";
+			}, false);
+			inputs[i].addEventListener('focusout', function(){
+				this.previousElementSibling.style.color = "#595964";
+			}, false);
+		}
+
 }, false);
